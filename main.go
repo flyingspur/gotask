268517088471a432387d47a5dd070ed22118a09a
flyingspur <seenu.ramachandran@gmail.com>
2017-06-16 23:20:41 +0530

    adding get tasks

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ type Task struct {
 	Created  time.Time `json:"created"`
 }
 
-var taskhash = make(map[int]string)
+var taskhash = make(map[int][]string)
 
 func PostTaskHandler(w http.ResponseWriter, r *http.Request) {
 	var task Task
@@ -21,9 +21,19 @@ func PostTaskHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	taskhash[task.TaskDate] = task.TaskDesc
+	taskhash[task.TaskDate] = append(taskhash[task.TaskDate], task.TaskDesc)
 	task.Created = time.Now()
 	j, err := json.Marshal(task)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(j)
+}
+
+func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+	j, err := json.Marshal(taskhash)
 	if err != nil {
 		panic(err)
 	}
@@ -35,6 +45,7 @@ func PostTaskHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	t := mux.NewRouter().StrictSlash(false)
 	t.HandleFunc("/api/task", PostTaskHandler).Methods("POST")
-	server := &http.Server{Addr:":8080", Handler: t,}
+	t.HandleFunc("/api/task", GetTasksHandler).Methods("GET")
+	server := &http.Server{Addr: ":8080", Handler: t}
 	server.ListenAndServe()
 }
