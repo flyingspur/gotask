5f1b179889027059578a47f785436dc8b9f634e5
flyingspur <seenu.ramachandran@gmail.com>
2017-06-16 21:49:23 +0530

    working post task

--- main.go
+++ main.go
@@ -1,20 +1,40 @@
 package main
 
-import ("fmt"
-		"net/http"
-		"github.com/gorilla/mux"
-		"time"
-		"encoding/json"
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+	"time"
 )
 
 type Task struct {
-	TaskDate 	string 	`json:"date"`
-	TaskDesc	string		`json:"desc"`
-	Created		time.Time	`json:"created"`
+	TaskDate int       `json:"date"`
+	TaskDesc string    `json:"desc"`
+	Created  time.Time `json:"created"`
 }
 
-var task = make(map[string]string)
+var taskhash = make(map[int]string)
 
-func PostTask(w http.ResponseWriter, r *http.Request) {
+func PostTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	err := json.NewDecoder(r.Body).Decode(&task)
+	if err != nil {
+		panic(err)
+	}
+	taskhash[task.TaskDate] = task.TaskDesc
+	task.Created = time.Now()
+	j, err := json.Marshal(task)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(j)
+}
 
+func main() {
+	t := mux.NewRouter().StrictSlash(false)
+	t.HandleFunc("/api/task", PostTaskHandler).Methods("POST")
+	server := &http.Server{Addr:":8080", Handler: t,}
+	server.ListenAndServe()
 }
