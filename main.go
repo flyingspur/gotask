1b293dc8a93f1763717ad8f188b6ee15eac66205
flyingspur <seenu.ramachandran@gmail.com>
2017-06-19 16:13:53 +0530

    redesigning

--- main.go
+++ main.go
@@ -3,49 +3,69 @@ package main
 import (
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
-	"time"
 )
 
-type PostTask struct {
-	TaskDate int64     `json:"date"`
-	TaskDesc []string  `json:"desc"`
-	Created  time.Time `json:"created"`
+type TaskList []struct {
+	TaskDate string `json:"date"`
+	//TaskDesc []string `json:"tasks"`
+	TaskDesc []map[string]string `json:"tasks"`
 }
 
-var taskmap = make(map[int64][]string)
+var taskmap = make(map[string]interface{})
+var item = make(map[string]string)
 
-func PostTaskHandler(w http.ResponseWriter, r *http.Request) {
+func PostTasks(w http.ResponseWriter, r *http.Request) {
 
-	var msg PostTask
-	err := json.NewDecoder(r.Body).Decode(&msg)
-	if err != nil {
+	var msg TaskList
+	if err := json.NewDecoder(r.Body).Decode(&msg); err != nil {
 		panic(err)
 	}
-	msg.Created = time.Now()
-	for _, v := range msg.TaskDesc {
-		taskmap[msg.TaskDate] = append(taskmap[msg.TaskDate], v)
+	for _, dates := range msg {
+		for _, tasks := range dates.TaskDesc {
+			for ktaskitem, taskitem := range tasks {
+				item[ktaskitem] = taskitem
+			}
+		}
+		taskmap[dates.TaskDate] = item
 	}
-	j, err := json.Marshal(msg)
+
+	j, _ := json.Marshal(msg)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	w.Write(j)
 }
 
-func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
-	j, err := json.Marshal(taskmap)
+func GetTasks(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	date := vars["date"]
+	task := vars["task"]
+
+	log.Println(date, task)
+	log.Println(taskmap[date])
+	//if date != nil {
+	//	if task != nil {
+	//		parsestring = taskmap
+	j, err := json.Marshal(taskmap[date])
 	if err != nil {
 		panic(err)
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
+	w.WriteHeader(http.StatusOK)
 	w.Write(j)
+}
+
+func DeleteTasks(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
 	t := mux.NewRouter().StrictSlash(false)
-	t.HandleFunc("/api/task", PostTaskHandler).Methods("POST")
-	t.HandleFunc("/api/task", GetTasksHandler).Methods("GET")
+	t.HandleFunc("/api/task", PostTasks).Methods("POST")
+	t.HandleFunc("/api/task", GetTasks).Methods("GET")
+	t.HandleFunc("/api/task/{date}/{task}", GetTasks).Methods("GET")
+	t.HandleFunc("/api/task/{date}", GetTasks).Methods("GET")
+	t.HandleFunc("/api/task", DeleteTasks).Methods("DELETE")
 	server := &http.Server{Addr: ":8080", Handler: t}
 	server.ListenAndServe()
 }
